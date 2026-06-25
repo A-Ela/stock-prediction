29d1a8109464904f23017eac62f05e6d5e895152
A-Ela <a.eltahir005@gmail.com>
2026-06-25 21:24:45 +0800

    deployment-changes

--- backend/server.js
+++ backend/server.js
@@ -36,8 +36,9 @@ const start = async () => {
     );
   }
 
-  app.listen(5000, () => {
-    console.log("Server running on port 5000");
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
   });
 };
 
